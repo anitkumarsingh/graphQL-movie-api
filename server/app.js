ed443dba1516c1b7cf09412178ae9b5866c7bd82
anitkumarsingh <anikumarsingh99@gmail.com>
2022-01-15 20:37:25 +0530

    graphql setup

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const {graphqlHTTP} = require('express-graphql');
+
 dotenv.config({path:'../config/env'});
 
 const PORT = process.env.PORT || 5000
 
 const app = express();
+
+app.use('/graphql',graphqlHTTP({
+
+}))
 
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV}`)
