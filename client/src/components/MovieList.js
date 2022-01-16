4bba81f373b2015e7204677d5a5402b8b33ad35a
anitkumarsingh <anikumarsingh99@gmail.com>
2022-01-16 14:14:15 +0530

    rendering data to the compoment

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -14,10 +14,19 @@ export const MovieList = () => {
 const {loading,error,data} = useQuery(MOVIES_QUERY);
 if(loading) return <p>Loading...</p>
 console.log(data)
+const renderMovies = ()=> {
+  return(
+    data.movies.map(m=>{
+      return(
+        <li key={m.id}>{m.name}</li>
+      )
+    })
+  )
+}
   return (
     <div>
       <ul>
-        <li>Movie List</li>
+        {renderMovies()}
       </ul>
     </div>
   )
