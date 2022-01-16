0e1448001154c587fe49e66700de9fb7488d8e9c
anitkumarsingh <anikumarsingh99@gmail.com>
2022-01-16 13:50:08 +0530

    apollo client setup

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,12 +1,19 @@
-import { MovieList } from "../components/MovieList";
+import { MovieList } from '../components/MovieList';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BASE_URL } from '../constant';
 
-const App = () =>{
-  return (
-   <div>
-     <h1>Watch List</h1>
-     <MovieList/>
-   </div>
-  );
-}
+const client = new ApolloClient({
+	uri: `${BASE_URL}/graphql`,
+	cache: new InMemoryCache()
+});
+
+const App = () => {
+	return (
+		<ApolloProvider client={client}>
+			<h1>Watch List</h1>
+			<MovieList />
+		</ApolloProvider>
+	);
+};
 
 export default App;
